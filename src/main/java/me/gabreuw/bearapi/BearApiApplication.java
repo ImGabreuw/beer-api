82bfd5048f96b94e5e19c758cbd6b4d67ec7c7a7
ImGabreuw <imgabreuw.dev@gmail.com>
2021-06-13 18:50:28 -0300

    Project setup

---
+++ src/main/java/me/gabreuw/bearapi/BearApiApplication.java
@@ -0,0 +1,13 @@
+package me.gabreuw.bearapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BearApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/gabreuw/bearapi/BearApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gabreuw.bearapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BearApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
